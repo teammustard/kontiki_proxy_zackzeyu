0a24ca25c0b6585e5a6a739211bc51258ec3f236
Zack Zhu <zeyuzhu1@gmail.com>
2019-05-31 15:20:49 -0400

    Add prod/dev switch

--- server/index.js
+++ server/index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3010;
 const path = require('path');
+const ENV = process.env.NODE_ENV;
 
-app.use(express.static(path.join(__dirname, '../public')));
+const servedFolder = ENV === 'production' ? '../public' : '../dist';
+
+app.use(express.static(path.join(__dirname, servedFolder)));
 
 app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, '../public/index.html'));
+	res.sendFile(path.join(__dirname, servedFolder, 'index.html'));
 });
 
-app.listen(PORT, () => console.log(`🚀 Proxy Server ready at port ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 ${ENV} Proxy Server ready at port ${PORT}`));
