4bcac30c095051bc2fcdf8e8a7f55efc84e16ce6
Zack Zhu <zeyuzhu1@gmail.com>
2019-05-31 14:17:45 -0400

    Rename folder

--- server/index.js
+++ server/index.js
@@ -3,10 +3,10 @@ const app = express();
 const PORT = process.env.PORT || 3010;
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, '../dist/index.html'));
+	res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 app.listen(PORT, () => console.log(`🚀 Proxy Server ready at port ${PORT}`));
